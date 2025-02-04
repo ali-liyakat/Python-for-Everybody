a5e180c80e2f26a8aa04902d281c2cc1f9c6d652
ali liyakat <aliliyakat902@gmail.com>
2025-02-04 16:18:41 +0800

    All about functions.

---
+++ 03-functions.py
@@ -0,0 +1,15 @@
+# Functions in Python
+
+def print_message():                    # function definition
+    print("Welcome to the course")
+    print("Happy Learning")
+
+print_message()         # function call
+
+
+def add(a, b):      # a and b are called parameters
+    return a + b        # returns the result to function call
+
+res = add(4, 5)        # 4 and 5 are called arguments
+print(res)
+
